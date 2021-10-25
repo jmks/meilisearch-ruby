0a40f7a78eca0d2651a5b707a4f4c43e9d48f9af
Jason Schweier <jasons@nulogy.com>
2021-10-25 18:42:25 -0400

    Create matcher for list of synonyms

---
+++ spec/support/matchers/synonyms.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# This matches the hash returned by index#synonyms
+# Meilisearch can return the list of synonyms in any order
+RSpec::Matchers.define :match_synonyms do |expected|
+  match do |actual|
+    matching_keys?(actual, expected) && matching_values?(actual, expected)
+  end
+
+  private
+
+  def matching_keys?(actual, expected)
+    actual_keys = actual.keys.map(&:to_s)
+    expected_keys = expected.keys.map(&:to_s)
+
+    actual_keys.sort == expected_keys.sort
+  end
+
+  def matching_values?(actual, expected)
+    actual.all? do |key, synonyms|
+      synonym = key.to_s
+      expected_synonyms = expected[key.to_sym] || expected[synonym] || []
+
+      synonyms.sort == expected_synonyms.sort
+    end
+  end
+end
